a409e03a822ce7a7129ee9da22cadb5a40c9caed
Amay Moon <amaylemoon@gmail.com>
2023-03-09 18:25:35 +0100

    ex02

---
+++ ex02/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "ft_abs.h"
+
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = -5;
+	b = 10;
+	c = 0;
+	printf("la valeur absolue de %d est : %d\n", a, ABS(a)));
+	printf("la valeur absolue de %d est : %d\n", b, ABS(b)));
+	printf("la valeur absolue de %d est : %d\n", c, ABS(c)));
+	return (0);
+}
